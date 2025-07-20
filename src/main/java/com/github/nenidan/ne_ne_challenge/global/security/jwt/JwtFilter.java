a8b4d21cf59ea90bebf0d41f2f1ab686d88b398c
huouvcti <huouvcti@gamil.com>
2025-07-20 23:54:15 +0900

    feat: JwtFilter 구현

---
+++ src/main/java/com/github/nenidan/ne_ne_challenge/global/security/jwt/JwtFilter.java
@@ -0,0 +1,81 @@
+package com.github.nenidan.ne_ne_challenge.global.security.jwt;
+
+import com.github.nenidan.ne_ne_challenge.global.security.auth.Auth;
+import com.github.nenidan.ne_ne_challenge.global.security.exception.CustomSecurityException;
+import com.github.nenidan.ne_ne_challenge.global.security.exception.SecurityServletErrorCode;
+import com.github.nenidan.ne_ne_challenge.global.security.util.SecurityResponseWriter;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain chain
+    ) throws ServletException, IOException {
+
+        String bearerJwt = request.getHeader("Authorization");
+
+        if (!JwtUtil.isValidBearerToken(bearerJwt)) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        String jwt = jwtUtil.extractToken(bearerJwt);
+
+        try {
+
+            Claims claims = jwtUtil.extractClaims(jwt);
+
+            if (claims == null) {
+                response.sendError(SecurityServletErrorCode.INVALID_JWT.getHttpStatus(), SecurityServletErrorCode.INVALID_JWT.getMessage());
+                return;
+            }
+
+            Auth auth = jwtUtil.extractAuth(claims);
+
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                    auth, null, List.of(new SimpleGrantedAuthority(auth.getRole().getAuthority()))
+            );
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+
+            chain.doFilter(request, response);
+
+        } catch (Exception e) {
+            SecurityServletErrorCode errorCode = mapJwtException(e);
+            SecurityResponseWriter.writeJsonErrorResponse(response, errorCode);
+        }
+    }
+
+    private SecurityServletErrorCode mapJwtException(Exception e) {
+        if (e instanceof SecurityException || e instanceof MalformedJwtException || e instanceof CustomSecurityException) {
+            return SecurityServletErrorCode.INVALID_JWT_SIGNATURE;
+        } else if (e instanceof ExpiredJwtException) {
+            return SecurityServletErrorCode.EXPIRED_JWT_TOKEN;
+        } else if (e instanceof UnsupportedJwtException) {
+            return SecurityServletErrorCode.UNSUPPORTED_JWT;
+        } else {
+            return SecurityServletErrorCode.INTERNAL_SERVER_ERROR;
+        }
+    }
+}
