f1268e39ffaabf0d927b2c477794981f6391e112
huouvcti <huouvcti@gamil.com>
2025-07-20 23:55:24 +0900

    refactor: 사용하지 않는 import 문 제거

--- src/main/java/com/github/nenidan/ne_ne_challenge/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/github/nenidan/ne_ne_challenge/global/exception/GlobalExceptionHandler.java
@@ -5,7 +5,6 @@ import com.github.nenidan.ne_ne_challenge.global.security.exception.CustomSecuri
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
