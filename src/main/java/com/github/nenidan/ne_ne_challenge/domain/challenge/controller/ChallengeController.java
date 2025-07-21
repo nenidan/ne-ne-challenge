8e7216efdf2303b7c92ee63954bd37ef1e39268f
Jaehyeon Han <jaehyeonhan99@gmail.com>
2025-07-21 20:00:22 +0900

    feat(challenge): 챌린지 조회 기능 추가

--- src/main/java/com/github/nenidan/ne_ne_challenge/domain/challenge/controller/ChallengeController.java
+++ src/main/java/com/github/nenidan/ne_ne_challenge/domain/challenge/controller/ChallengeController.java
@@ -1,18 +1,19 @@
 package com.github.nenidan.ne_ne_challenge.domain.challenge.controller;
 
+import com.github.nenidan.ne_ne_challenge.domain.challenge.dto.request.ChallengeSearchCond;
 import com.github.nenidan.ne_ne_challenge.domain.challenge.dto.request.CreateChallengeRequest;
 import com.github.nenidan.ne_ne_challenge.domain.challenge.dto.response.ChallengeResponse;
 import com.github.nenidan.ne_ne_challenge.domain.challenge.service.ChallengeService;
 import com.github.nenidan.ne_ne_challenge.global.dto.ApiResponse;
+import com.github.nenidan.ne_ne_challenge.global.dto.CursorResponse;
 import com.github.nenidan.ne_ne_challenge.global.security.auth.Auth;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/api")
@@ -28,6 +29,22 @@ public class ChallengeController {
         return ApiResponse.success(HttpStatus.CREATED,
             "챌린지를 생성했습니다.",
             challengeService.createChallenge(request, auth.getId())
+        );
+    }
+
+    @GetMapping("/challenges/{id}")
+    public ResponseEntity<ApiResponse<ChallengeResponse>> getChallenge(@PathVariable Long id) {
+        return ApiResponse.success(HttpStatus.OK,
+            "챌린지를 조회했습니다.",
+            challengeService.getChallenge(id)
+        );
+    }
+
+    @GetMapping("/challenges")
+    public ResponseEntity<ApiResponse<CursorResponse<ChallengeResponse, LocalDateTime>>> getChallengeList(@ModelAttribute ChallengeSearchCond cond) {
+        return ApiResponse.success(HttpStatus.OK,
+            "챌린지 목록을 조회했습니다.",
+            challengeService.getChallengeList(cond)
         );
     }
 }
--- src/main/java/com/github/nenidan/ne_ne_challenge/domain/challenge/service/ChallengeService.java
+++ src/main/java/com/github/nenidan/ne_ne_challenge/domain/challenge/service/ChallengeService.java
@@ -1,16 +1,23 @@
 package com.github.nenidan.ne_ne_challenge.domain.challenge.service;
 
+import com.github.nenidan.ne_ne_challenge.domain.challenge.dto.request.ChallengeSearchCond;
 import com.github.nenidan.ne_ne_challenge.domain.challenge.dto.request.CreateChallengeRequest;
 import com.github.nenidan.ne_ne_challenge.domain.challenge.dto.response.ChallengeResponse;
 import com.github.nenidan.ne_ne_challenge.domain.challenge.entity.Challenge;
+import com.github.nenidan.ne_ne_challenge.domain.challenge.exception.ChallengeErrorCode;
+import com.github.nenidan.ne_ne_challenge.domain.challenge.exception.ChallengeException;
 import com.github.nenidan.ne_ne_challenge.domain.challenge.repository.ChallengeRepository;
 import com.github.nenidan.ne_ne_challenge.domain.user.entity.User;
 import com.github.nenidan.ne_ne_challenge.domain.user.exception.UserErrorCode;
 import com.github.nenidan.ne_ne_challenge.domain.user.exception.UserException;
 import com.github.nenidan.ne_ne_challenge.domain.user.repository.UserRepository;
+import com.github.nenidan.ne_ne_challenge.global.dto.CursorResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
@@ -32,5 +39,34 @@ public class ChallengeService {
         challengeUserService.joinChallenge(user.getId(), savedChallenge.getId(), true);
 
         return ChallengeResponse.from(savedChallenge);
+    }
+
+    public ChallengeResponse getChallenge(Long id) {
+        Challenge foundChallenge = challengeRepository.findById(id).orElseThrow(() -> new ChallengeException(
+            ChallengeErrorCode.CHALLENGE_NOT_FOUND));
+
+        return ChallengeResponse.from(foundChallenge);
+    }
+
+    public CursorResponse<ChallengeResponse, LocalDateTime> getChallengeList(ChallengeSearchCond cond) {
+        int size = cond.getSize();
+
+        List<ChallengeResponse> challengeList = challengeRepository.getChallengeList(cond.getUserId(),
+            cond.getName(),
+            cond.getStatus(),
+            cond.getDueAt(),
+            cond.getCategory(),
+            cond.getMaxParticipationFee(),
+            cond.getCursor(),
+            size + 1
+        ).stream()
+            .map(ChallengeResponse::from)
+            .toList();
+
+        boolean hasNext = challengeList.size() > size;
+        List<ChallengeResponse> content = hasNext ? challengeList.subList(0, size) : challengeList;
+        LocalDateTime nextCursor = hasNext ? challengeList.get(challengeList.size() - 1).getCreatedAt() : null;
+
+        return CursorResponse.of(content, nextCursor, hasNext);
     }
 }
