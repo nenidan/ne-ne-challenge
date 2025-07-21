d8887ec6d6cedefac68aeef4a31388c3340cce61
NewJKH <newsun0805@gmail.com>
2025-07-21 17:48:11 +0900

    fix(Notification): "알림 상태변경 @NoArgsConstructor 누락으로 인한 추가"

--- src/main/java/com/github/nenidan/ne_ne_challenge/domain/notification/dto/request/ReadNotificationRequest.java
+++ src/main/java/com/github/nenidan/ne_ne_challenge/domain/notification/dto/request/ReadNotificationRequest.java
@@ -2,9 +2,11 @@ package com.github.nenidan.ne_ne_challenge.domain.notification.dto.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class ReadNotificationRequest {
 	boolean isRead;
 }
