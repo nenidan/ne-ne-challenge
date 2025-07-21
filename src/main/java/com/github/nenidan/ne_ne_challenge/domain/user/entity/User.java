348cf7bf166255ecb3e8e4c206f03dd55d636939
huouvcti <huouvcti@gamil.com>
2025-07-21 13:10:18 +0900

    fix(User): User 엔티티 수정
    - @Column(nullable = false)
    - @Enumerated(EnumType.STRING)

--- src/main/java/com/github/nenidan/ne_ne_challenge/domain/user/entity/User.java
+++ src/main/java/com/github/nenidan/ne_ne_challenge/domain/user/entity/User.java
@@ -24,13 +24,17 @@ public class User extends BaseEntity {
     @Column(nullable = false, unique = true)
     private String email;
 
+    @Column(nullable = false)
     private String password;
 
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
     private UserRole role;
 
     @Column(nullable = false, unique = true)
     private String nickname;
 
+    @Column(nullable = false)
     private LocalDate birth;
 
     private String bio;
