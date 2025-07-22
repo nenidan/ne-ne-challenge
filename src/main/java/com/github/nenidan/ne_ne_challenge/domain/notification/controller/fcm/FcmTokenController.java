e2bb607352bac1015f6f8c1e3cae357b11311e2f
NewJKH <newsun0805@gmail.com>
2025-07-22 20:16:44 +0900

    feat(Notification): "FCM save 기능 추가"

--- src/main/java/com/github/nenidan/ne_ne_challenge/domain/notification/controller/fcm/FcmTokenController.java
+++ src/main/java/com/github/nenidan/ne_ne_challenge/domain/notification/controller/fcm/FcmTokenController.java
@@ -1,7 +1,17 @@
 package com.github.nenidan.ne_ne_challenge.domain.notification.controller.fcm;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.github.nenidan.ne_ne_challenge.domain.notification.dto.fcm.request.FcmTokenRequest;
+import com.github.nenidan.ne_ne_challenge.domain.notification.service.fcm.FcmTokenService;
+import com.github.nenidan.ne_ne_challenge.global.dto.ApiResponse;
+import com.github.nenidan.ne_ne_challenge.global.security.auth.Auth;
 
 import lombok.RequiredArgsConstructor;
 
@@ -9,5 +19,14 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @RequestMapping("/api")
 public class FcmTokenController {
+	private final FcmTokenService fcmTokenService;
 
+	@PostMapping("/fcm/token")
+	public ResponseEntity<ApiResponse<Void>> saveToken(@AuthenticationPrincipal Auth auth, @RequestBody FcmTokenRequest request) {
+		return ApiResponse.success(
+			HttpStatus.OK,
+			"FCM 토큰 저장완료",
+			fcmTokenService.saveToken(auth.getId(), request.getToken())
+		);
+	}
 }
