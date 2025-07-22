ad671a5bdbecef1481c05a0361a551e6aba8a8e5
dogyun00 <kimdk0903@naver.com>
2025-07-22 14:17:58 +0900

    feat(point): 지갑 생성 서비스 로직 구현

--- src/main/java/com/github/nenidan/ne_ne_challenge/domain/point/service/PointService.java
+++ src/main/java/com/github/nenidan/ne_ne_challenge/domain/point/service/PointService.java
@@ -1,7 +1,36 @@
 package com.github.nenidan.ne_ne_challenge.domain.point.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.github.nenidan.ne_ne_challenge.domain.point.dto.response.PointBalanceResponse;
+import com.github.nenidan.ne_ne_challenge.domain.point.dto.response.PointResponse;
+import com.github.nenidan.ne_ne_challenge.domain.point.entity.PointWallet;
+import com.github.nenidan.ne_ne_challenge.domain.point.exception.PointErrorCode;
+import com.github.nenidan.ne_ne_challenge.domain.point.exception.PointException;
+import com.github.nenidan.ne_ne_challenge.domain.point.repository.PointWalletRepository;
+import com.github.nenidan.ne_ne_challenge.domain.user.entity.User;
+import com.github.nenidan.ne_ne_challenge.domain.user.exception.UserErrorCode;
+import com.github.nenidan.ne_ne_challenge.domain.user.exception.UserException;
+import com.github.nenidan.ne_ne_challenge.domain.user.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class PointService {
+
+    private final UserRepository userRepository;
+    private final PointWalletRepository pointWalletRepository;
+
+    @Transactional
+    public void createWallet(Long userId) {
+
+        User findUser = userRepository.findById(userId)
+            .orElseThrow(() -> new UserException(UserErrorCode.USER_NOT_FOUND));
+
+        PointWallet pointWallet = new PointWallet(findUser);
+        pointWalletRepository.save(pointWallet);
+    }
 }
