91d47ae2a2b8a33bc453594247997876c6e56620
NewJKH <newsun0805@gmail.com>
2025-07-23 17:00:42 +0900

    refactor(Notification): "팀내 컨벤션에 따라 userId 로 변경"

--- src/main/java/com/github/nenidan/ne_ne_challenge/domain/notification/controller/fcm/FcmTokenController.java
+++ src/main/java/com/github/nenidan/ne_ne_challenge/domain/notification/controller/fcm/FcmTokenController.java
@@ -2,7 +2,7 @@ package com.github.nenidan.ne_ne_challenge.domain.notification.controller.fcm;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.github.nenidan.ne_ne_challenge.domain.notification.dto.fcm.request.FcmTokenRequest;
 import com.github.nenidan.ne_ne_challenge.domain.notification.service.fcm.FcmTokenService;
 import com.github.nenidan.ne_ne_challenge.global.dto.ApiResponse;
-import com.github.nenidan.ne_ne_challenge.global.security.auth.Auth;
 
 import lombok.RequiredArgsConstructor;
 
@@ -21,12 +20,12 @@ import lombok.RequiredArgsConstructor;
 public class FcmTokenController {
 	private final FcmTokenService fcmTokenService;
 
-	@PostMapping("/fcm/token")
-	public ResponseEntity<ApiResponse<Void>> saveToken(@AuthenticationPrincipal Auth auth, @RequestBody FcmTokenRequest request) {
+	@PostMapping("/fcm/token/{userId}")
+	public ResponseEntity<ApiResponse<Void>> saveToken(@PathVariable Long userId, @RequestBody FcmTokenRequest request) {
 		return ApiResponse.success(
 			HttpStatus.OK,
 			"FCM 토큰 저장완료",
-			fcmTokenService.saveToken(auth.getId(), request.getToken())
+			fcmTokenService.saveToken(userId, request.getToken())
 		);
 	}
 }
