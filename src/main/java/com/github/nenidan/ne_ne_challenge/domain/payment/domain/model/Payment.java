ef1c6990f74fa6deca30d7abaf50acd3077be33e
dogyun00 <kimdk0903@naver.com>
2025-07-29 01:35:11 +0900

    feat(payment): 토스페이먼츠 key-in 결제 연동을 위한 Payment 엔티티 필드 추가

--- src/main/java/com/github/nenidan/ne_ne_challenge/domain/payment/domain/model/Payment.java
+++ src/main/java/com/github/nenidan/ne_ne_challenge/domain/payment/domain/model/Payment.java
@@ -1,6 +1,5 @@
 package com.github.nenidan.ne_ne_challenge.domain.payment.domain.model;
 
-import com.github.nenidan.ne_ne_challenge.domain.payment.domain.type.PaymentMethod;
 import com.github.nenidan.ne_ne_challenge.domain.payment.domain.type.PaymentStatus;
 import com.github.nenidan.ne_ne_challenge.domain.payment.exception.PaymentErrorCode;
 import com.github.nenidan.ne_ne_challenge.domain.payment.exception.PaymentException;
@@ -25,21 +24,28 @@ public class Payment extends BaseEntity {
     private Long userId;
 
     @Column(nullable = false)
-    private int amount;
+    private int amount; // TOSS : totalAmount
+
+    @Column(name = "payment_method", nullable = false)
+    private String paymentMethod; // TOSS : method
+
+    @Column(name = "payment_key", unique = true)
+    private String paymentKey; // TOSS : paymentKey -> nullable : key - in 호출 전에는 null
+
+    @Column(name = "order_id", nullable = false, unique = true)
+    private String orderId; // TOSS : orderId
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
-    private PaymentStatus status;
+    private PaymentStatus status; // TOSS : status
 
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    private PaymentMethod method;
+    @Column(name = "requested_at", nullable = false)
+    private LocalDateTime requestedAt; // TOSS : requestedAt
 
-    @Column(nullable = false)
-    private LocalDateTime requestedAt; // 결제 요청 시각
+    @Column(name = "approved_at")
+    private LocalDateTime approvedAt; // TOSS : approvedAt 결제 승인(성공) 시간
 
-    private LocalDateTime confirmedAt; // 결제 완료 시각
-
+    @Column(name = "failed_ar")
     private LocalDateTime failedAt; // 결제 실패 시각
 
     private Payment(Long userId, int amount, PaymentStatus status, PaymentMethod method, LocalDateTime requestedAt) {
