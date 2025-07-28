b617a02d6076709e843b0d08461e9bd3cb001adb
dogyun00 <kimdk0903@naver.com>
2025-07-28 16:27:34 +0900

    feat(point): PointClient를 global 패키지에 생성

---
+++ src/main/java/com/github/nenidan/ne_ne_challenge/global/client/point/PointClient.java
@@ -0,0 +1,7 @@
+package com.github.nenidan.ne_ne_challenge.global.client.point;
+
+
+public interface PointClient {
+
+    void createPointWallet(Long userId);
+}
---
+++ src/main/java/com/github/nenidan/ne_ne_challenge/global/client/point/PointClientImpl.java
@@ -0,0 +1,38 @@
+package com.github.nenidan.ne_ne_challenge.global.client.point;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+import jakarta.annotation.PostConstruct;
+
+@Component
+public class PointClientImpl implements PointClient {
+
+    @Value("${external.base-url}")
+    private String baseUrl;
+
+    private RestClient restClient;
+
+    @PostConstruct
+    public void init() {
+        restClient = RestClient.builder()
+            .baseUrl(baseUrl)
+            .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+            .build();
+    }
+
+    @Override
+    public void createPointWallet(Long userId) {
+        restClient.post()
+            .uri(uriBuilder -> uriBuilder.path("/internal/points/wallet")
+                .queryParam("userId", userId)
+                .build()
+            )
+            .retrieve()
+            .toBodilessEntity();
+    }
+}
