b97df666fd714e6bad13cf4ebee2f14ce6b00d90
NewJKH <newsun0805@gmail.com>
2025-07-29 16:46:01 +0900

    refactor(Notification): '설정 분리 + Firebase.json 인식 가능하게 변경'

--- src/main/java/com/github/nenidan/ne_ne_challenge/global/config/FirebaseConfig.java
+++ src/main/java/com/github/nenidan/ne_ne_challenge/global/config/FirebaseConfig.java
@@ -3,6 +3,7 @@ package com.github.nenidan.ne_ne_challenge.global.config;
 import java.io.FileInputStream;
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 import com.google.auth.oauth2.GoogleCredentials;
@@ -13,10 +14,13 @@ import jakarta.annotation.PostConstruct;
 
 @Configuration
 public class FirebaseConfig {
+	@Value("${firebase.config.path}")
+	private String firebaseConfigPath;
+
 	@PostConstruct
 	public void init() throws IOException {
 		FileInputStream serviceAccount =
-			new FileInputStream("src/main/resources/firebase/firebase-key.json");
+			new FileInputStream(firebaseConfigPath);
 
 		FirebaseOptions options = new FirebaseOptions.Builder()
 			.setCredentials(GoogleCredentials.fromStream(serviceAccount))
