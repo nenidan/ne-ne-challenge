2cca931fb782bd3632039f2020e7355e3c7500f5
kimyongjun0129 <kimyongjun0129@naver.com>
2025-08-07 00:48:11 +0900

    docs(product): product 캐시 로직에 대한 설명 주석 작성

--- src/main/java/com/github/nenidan/ne_ne_challenge/domain/shop/product/infrastructure/repository/ProductCacheService.java
+++ src/main/java/com/github/nenidan/ne_ne_challenge/domain/shop/product/infrastructure/repository/ProductCacheService.java
@@ -31,6 +31,18 @@ public class ProductCacheService {
     public static String PRODUCT_CACHE_KEY = "product_list";
     public static int PRODUCT_CACHE_SIZE = 30;
 
+    /**
+     * 상품 첫 페이지 데이터를 캐시로 갱신합니다.
+     * <p>
+     * 이 메서드는 AOP 프록시 객체를 통해 {@link ProductRepositoryImpl#findAllByCursor(Long, int, String)} 메서드를 호출하여,
+     * 첫 페이지에 해당하는 상품 목록을 조회하고, 해당 결과를 캐시 이름 {@code PRODUCT_CACHE_NAME}의
+     * {@code PRODUCT_CACHE_KEY}에 저장합니다.
+     * <p>
+     * 주의: {@code findAllByCursor()}는 {@code @Cacheable} 어노테이션이 적용되어 있기 때문에,
+     * 동일 클래스 내부에서 직접 호출하면 캐싱이 적용되지 않습니다.
+     * 따라서 Spring AOP 프록시 객체를 통해 호출해야 합니다.
+     *
+     */
     public void refreshFirstPageCache() {
         ProductRepositoryImpl proxy = applicationContext.getBean(ProductRepositoryImpl.class);
         List<Product> productList = proxy.findAllByCursor(null, PRODUCT_CACHE_SIZE, null);
