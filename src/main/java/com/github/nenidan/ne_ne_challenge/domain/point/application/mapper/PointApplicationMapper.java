7c9feb3b653e1e37f7fddf5034c36ed3fb9604bf
dogyun00 <kimdk0903@naver.com>
2025-08-17 01:13:10 +0900

    refactor(point): 사용하지 않는 메서드 삭제

--- src/main/java/com/github/nenidan/ne_ne_challenge/domain/point/application/mapper/PointApplicationMapper.java
+++ src/main/java/com/github/nenidan/ne_ne_challenge/domain/point/application/mapper/PointApplicationMapper.java
@@ -1,19 +1,9 @@
 package com.github.nenidan.ne_ne_challenge.domain.point.application.mapper;
 
 import com.github.nenidan.ne_ne_challenge.domain.point.application.dto.response.PointHistoryResult;
-import com.github.nenidan.ne_ne_challenge.domain.point.application.dto.response.PointWalletResult;
-import com.github.nenidan.ne_ne_challenge.domain.point.domain.PointTransaction;
-import com.github.nenidan.ne_ne_challenge.domain.point.domain.PointWallet;
+import com.github.nenidan.ne_ne_challenge.domain.point.domain.model.PointTransaction;
 
 public class PointApplicationMapper {
-
-    public static PointWalletResult toPointWalletResult(PointWallet wallet) {
-        return new PointWalletResult(
-            wallet.getId(),
-            wallet.getUserId(),
-            wallet.getBalance()
-        );
-    }
 
     public static PointHistoryResult toPointHistoryResult(PointTransaction transaction) {
         return new PointHistoryResult(
