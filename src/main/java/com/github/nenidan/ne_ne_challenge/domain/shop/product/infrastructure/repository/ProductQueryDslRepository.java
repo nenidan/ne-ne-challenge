e876a7ad135044549934509bb7b24b73cdb7e06f
kimyongjun0129 <kimyongjun0129@naver.com>
2025-08-17 02:29:56 +0900

    remove(product) : 사용하지 않는 QueryDSL 파일 삭제

--- src/main/java/com/github/nenidan/ne_ne_challenge/domain/shop/product/infrastructure/repository/ProductQueryDslRepository.java
+++
@@ -1,9 +0,0 @@
-package com.github.nenidan.ne_ne_challenge.domain.shop.product.infrastructure.repository;
-
-import java.util.List;
-
-import com.github.nenidan.ne_ne_challenge.domain.shop.product.infrastructure.entity.ProductEntity;
-
-public interface ProductQueryDslRepository {
-    List<ProductEntity> findAllProductsBy(Long cursor, String keyword, int limit);
-}
