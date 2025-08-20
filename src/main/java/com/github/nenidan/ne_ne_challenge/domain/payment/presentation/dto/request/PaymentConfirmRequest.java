53b42aebc9165af2d7136092ada4f3f01f2bab45
dogyun00 <kimdk0903@naver.com>
2025-08-20 18:56:26 +0900

    docs(payment): 결제 승인 API swagger 설정

--- src/main/java/com/github/nenidan/ne_ne_challenge/domain/payment/presentation/dto/request/PaymentConfirmRequest.java
+++ src/main/java/com/github/nenidan/ne_ne_challenge/domain/payment/presentation/dto/request/PaymentConfirmRequest.java
@@ -1,5 +1,6 @@
 package com.github.nenidan.ne_ne_challenge.domain.payment.presentation.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
@@ -10,12 +11,18 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class PaymentConfirmRequest {
 
+    @Schema(
+        description = "토스 결제 완료 후 토스에서 보내주는 결제 키, 임의의 값으로 요청을 보낼 수 없습니다.",
+        example = "tviva20250820163347aX8I6"
+    )
     @NotBlank(message = "결제 키는 필수입니다")
     private String paymentKey;
 
+    @Schema(description = "주문 ID", example = "order-80d814f7-3a43-4986-a47b-7e89c932680a")
     @NotBlank(message = "주문 ID는 필수입니다")
     private String orderId;
 
+    @Schema(description = "결제 금액", example = "10000")
     @Min(value = 10000, message = "최소 결제 금액은 10,000원입니다")
     @Max(value = 100000, message = "최대 결제 금액은 100,000원입니다")
     private int amount;
--- src/main/java/com/github/nenidan/ne_ne_challenge/domain/payment/presentation/dto/response/PaymentConfirmResponse.java
+++ src/main/java/com/github/nenidan/ne_ne_challenge/domain/payment/presentation/dto/response/PaymentConfirmResponse.java
@@ -2,6 +2,7 @@ package com.github.nenidan.ne_ne_challenge.domain.payment.presentation.dto.respo
 
 import java.time.LocalDateTime;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -9,9 +10,18 @@ import lombok.Getter;
 @Getter
 public class PaymentConfirmResponse {
 
+    @Schema(description = "주문 ID", example = "order-80d814f7-3a43-4986-a47b-7e89c932680a")
     private String orderId;
+
+    @Schema(description = "결제 금액", example = "10000")
     private int amount;
+
+    @Schema(description = "결제 수단", example = "단순 결제")
     private String method;
+
+    @Schema(description = "결제 상태", example = "DONE")
     private String status;
+
+    @Schema(description = "결제 승인 일시", example = "2025-08-20T07:48:05.261Z")
     private LocalDateTime approvedAt;
 }
