154efba5cb7c2639ae8a0b28720ca86c4c628429
dogyun00 <kimdk0903@naver.com>
2025-08-20 19:17:23 +0900

    docs(payment): 결제 조회 API swagger 설정 및 수정

--- src/main/java/com/github/nenidan/ne_ne_challenge/domain/payment/presentation/controller/external/PaymentController.java
+++ src/main/java/com/github/nenidan/ne_ne_challenge/domain/payment/presentation/controller/external/PaymentController.java
@@ -31,6 +31,8 @@ import com.github.nenidan.ne_ne_challenge.global.dto.CursorResponse;
 import com.github.nenidan.ne_ne_challenge.global.security.auth.Auth;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -47,11 +49,6 @@ public class PaymentController {
     private final PaymentFacade paymentFacade;
 
     /**
-     * 토스 결제 요청 전, orderId와 amount를 데이터베이스에 저장하기 위한 API
-     * @param auth 인증된 사용자 정보
-     * @param request amount(요청 가격)
-     * @return amount(요청 가격), orderId(UUID로 생성된 고유 ID), orderName(주문한 상품 이름 예: 포인트 10,000원)
-     *
      * 토스에서는 결제 요청 전 orderId와 amount를 세션이나 데이터베이스에 저장하는 것을 적극 권장한다.
      * @see <a href="https://docs.tosspayments.com/guides/v2/get-started/payment-flow#%EB%8D%94-%EC%95%8C%EC%95%84%EB%B3%B4%EA%B8%B0">토스페이먼츠 가이드</a>
      */
@@ -61,7 +58,7 @@ public class PaymentController {
     })
     @PostMapping("/payments/prepare")
     public ResponseEntity<ApiResponse<PaymentPrepareResponse>> preparePayment(
-        @AuthenticationPrincipal Auth auth,
+        @Parameter(hidden = true) @AuthenticationPrincipal Auth auth,
         @Valid @RequestBody PaymentPrepareRequest request
     ) {
 
@@ -75,12 +72,6 @@ public class PaymentController {
         );
     }
 
-    /**
-     * 토스페이 결제 승인 및 포인트 충전 API
-     * @param request 토스페이먼츠에서 제공한 결제 정보 (paymentKey, orderId, amount)
-     * @param auth 인증된 사용자 정보
-     * @return 결제 승인 결과 (orderId, amount, method, status, approvedAt)
-     */
     @Operation(summary = "결제 승인", description = "토스 결제 성공 후, 토스 페이먼츠에 승인 요청을 보내고, 포인트를 충전합니다.")
     @ApiResponses({
         @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "결제가 완료되었습니다. 포인트는 1~2분 이내로 충전될 예정입니다."),
@@ -88,7 +79,7 @@ public class PaymentController {
     @PostMapping("/payments/confirm")
     public ResponseEntity<ApiResponse<PaymentConfirmResponse>> confirmAndChargePoint(
         @Valid @RequestBody PaymentConfirmRequest request,
-        @AuthenticationPrincipal Auth auth) {
+        @Parameter(hidden = true) @AuthenticationPrincipal Auth auth) {
 
         PaymentConfirmResult paymentConfirmResult = paymentFacade.confirmAndChargePoint(
             auth.getId(),
@@ -102,20 +93,25 @@ public class PaymentController {
     }
 
     /**
-     * 결제 취소 API
      * 사용자는 자신이 결제한 포인트 중에서, 7일 이내의 사용되지 않은 포인트는 환불이 가능하다.
-     * @param auth 인증된 사용자 정보
-     * @param orderId 결제할 때 사용한 orderId
-     * @param request 환불 사유(예: 단순 변심)
-     * @return 결제 취소 결과
      */
-    @Operation(summary = "결제 취소", description = "7일 이내의 사용하지 않은 포인트를 결제 취소합니다.")
+    @Operation(
+        summary = "결제 취소",
+        description = "7일 이내의 사용하지 않은 포인트를 결제 취소합니다.",
+        parameters = {
+            @Parameter(
+                name = "orderId",
+                description = "결제 주문 ID",
+                required = true,
+                in = ParameterIn.PATH
+            )
+        })
     @ApiResponses({
         @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "결제 취소가 완료되었습니다."),
     })
     @PostMapping("/payments/{orderId}/cancel")
     public ResponseEntity<ApiResponse<PaymentCancelResponse>> cancelPayment(
-        @AuthenticationPrincipal Auth auth,
+        @Parameter(hidden = true) @AuthenticationPrincipal Auth auth,
         @PathVariable String orderId,
         @Valid @RequestBody PaymentCancelRequest request) {
 
@@ -129,15 +125,13 @@ public class PaymentController {
         );
     }
 
-    /**
-     * 나의 결제 내역 조회 API
-     * @param auth 인증된 사용자 정보
-     * @param request 결제 내역 검색 조건(cursor, size, status, startDate, endDate)
-     * @return 커서 기반의 자신의 결제 내역
-     */
+    @Operation(summary = "결제 내역 조회", description = "본인의 결제 내역을 조회합니다.")
+    @ApiResponses({
+        @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "결제 내역 조회가 완료되었습니다."),
+    })
     @GetMapping("/payments")
     public ResponseEntity<ApiResponse<CursorResponse<PaymentSearchResponse, Long>>> searchMyPayments(
-        @AuthenticationPrincipal Auth auth,
+        @Parameter(hidden = true) @AuthenticationPrincipal Auth auth,
         @Valid PaymentSearchRequest request) {
 
         CursorResponse<PaymentSearchResult, Long> result = paymentFacade.searchMyPayments(
