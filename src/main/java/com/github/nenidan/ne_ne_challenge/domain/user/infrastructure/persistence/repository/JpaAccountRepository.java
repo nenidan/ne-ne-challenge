f2c1ea40cb29016e31079349057b167b382fca58
gajicoding <gajicoding@gmail.com>
2025-08-21 13:28:11 +0900

    fix(User): 탈퇴한 회원 재 회원가입 시 500 에러 수정

--- src/main/java/com/github/nenidan/ne_ne_challenge/domain/user/infrastructure/persistence/repository/JpaAccountRepository.java
+++ src/main/java/com/github/nenidan/ne_ne_challenge/domain/user/infrastructure/persistence/repository/JpaAccountRepository.java
@@ -1,15 +1,13 @@
 package com.github.nenidan.ne_ne_challenge.domain.user.infrastructure.persistence.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.github.nenidan.ne_ne_challenge.domain.user.infrastructure.persistence.entity.AccountEntity;
+import org.springframework.data.jpa.repository.Query;
 
 public interface JpaAccountRepository extends JpaRepository<AccountEntity, Long> {
 
-    boolean existsByEmail(String email);
-
-    Optional<AccountEntity> findByEmail(String email);
+    @Query(value = "SELECT COUNT(*) FROM account WHERE email = :email", nativeQuery = true)
+    Long countByEmail(String email);
 
 }
